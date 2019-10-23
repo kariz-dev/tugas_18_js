e351f10339db8fcfcd57fb43cff7c5c6868fe88b
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-23 23:33:01 +0700

    tugas 18 js, Muhammad Rizwan

--- tugas18..js
+++ tugas18..js
@@ -4,7 +4,7 @@
 function tugasDelapanBelas(){
 // 1). Ambilah dua kata "long established"
     var kalimat = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.";
-    console.log("Tugas 1: ",kalimat.slice(8,23));
+    console.log("Tugas 1: ",kalimat.slice(8,24));
 
 // 2). Gabungkan kedua string menjadi satu variabel
     var string1 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
